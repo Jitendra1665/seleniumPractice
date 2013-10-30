f204cc2f146d3a252485a1e35b0af63949908ca0
Jitendra1665 <mail4jiten@gmail.com>
2013-10-30 20:43:33 +0530

    Update SampleJava.java

--- seleniumPractice/src/seleniumPractice/SampleJava.java
+++ seleniumPractice/src/seleniumPractice/SampleJava.java
@@ -3,7 +3,7 @@ package seleniumPractice;
 public class SampleJava {
 
 	public static void main(String[] args2) {
-		System.out.println("Hello world");
+		System.out.println("Hello world from github");
     
 	}
 

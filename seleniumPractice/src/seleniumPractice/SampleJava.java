ce99743d578a6e4ed9db6d8456f04545d2dbbbe4
Jitendra1665 <mail4jiten@gmail.com>
2013-10-30 20:01:48 +0530

    wer

--- seleniumPractice/src/seleniumPractice/SampleJava.java
+++ seleniumPractice/src/seleniumPractice/SampleJava.java
@@ -2,9 +2,9 @@ package seleniumPractice;
 
 public class SampleJava {
 
-	public static void main(String[] args1) {
+	public static void main(String[] args2) {
 		// TODO Auto-generated method stub
-
+    
 	}
 
 }
